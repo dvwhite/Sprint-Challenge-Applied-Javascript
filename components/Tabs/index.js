6fdecef7b26da99237448e407f0a0497277026d9
David White <dwhite0627@gmail.com>
2019-12-13 19:43:57 -0500

    Create tab component using dynamic data fetched using axios

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,27 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const apiURL = 'https://lambda-times-backend.herokuapp.com/topics';
+
+function Tab(topic) {
+    const divTag = document.createElement('div');
+    divTag.classList.add('tab');
+    divTag.textContent = topic;
+    return divTag;
+}
+
+function createTabs(topics) {
+    const topicsDiv = document.querySelector('.topics');
+    topics.forEach(topic => topicsDiv.appendChild(Tab(topic)));
+}
+
+axios.get(apiURL)
+    .then(response => {
+        const topicsArr = response.data.topics;
+        createTabs(topicsArr);
+        console.log(response);
+    })
+    .catch(err => {
+        console.log("Error:", err);
+    });
