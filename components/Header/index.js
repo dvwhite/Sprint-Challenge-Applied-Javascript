28a89227a56eb7f83d30486b093e481ec868cd43
David White <dwhite0627@gmail.com>
2019-12-13 19:31:34 -0500

    Create static header component, and helper function appendChildren

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,30 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    const outerDiv = document.createElement('div');
+    outerDiv.classList.add('header');
+
+    const spanDate = document.createElement('span');
+    spanTag.classList.add('date');
+    spanTag.textContent = 'SMARCH 28, 2019';
+
+    const h1Tag = document.createElement('h1');
+    h1Tag.textContent = 'Lambda Times';
+
+    const spanTemp = document.createElement('span');
+    spanTag.classList.add('temp');
+    spanTag.textContent = '98°';
+
+    outerDiv.appendChildren([spanDate, h1Tag, spanTemp]);
+    return outerDiv;
+}
+
+/*
+* Append an array of child nodes to a parent node
+* @param {Array} children: An array of child nodes
+* @param {node} parent: The parent node to append them to
+*/
+function appendChildren(children, parent) {
+    children.forEach(child => parent.appendNode(child));
+}
